0c3efdebebca7ba5143340f68203f7427e11df6a
Harsh <aparekh.harsh@gmail.com>
2025-04-21 17:54:14 +0530

    Start making this project

---
+++ SwiggyClone/Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2 className='tagline'>
+            Order food & Groceries <br/>Discover best restaurants.<br/>Swiggy it!
+            </h2>
+            
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
