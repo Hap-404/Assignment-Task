cc412aeaf8adfb0bba9c4447763316ad8b97c9a9
Harsh <harsh.parekh@aimdek.com>
2025-04-23 19:31:00 +0530

    Added Add Total price fucntionality

--- SwiggyClone/Frontend/src/context/StoreContext.jsx
+++ SwiggyClone/Frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
@@ -22,17 +22,24 @@ const StoreContextProvider = (props) => {
     const getTotalCartAmount = () => {
         let totalAmount = 0;
         for (const item in cartItems) {
-                let itemInfo = food_list.find((product)=>product._id === item)
-            }
-        }
-    }
+
+                    if (cartItems[item]>0){
+                        let itemInfo = food_list.find((product)=>product._id === item)
+                        totalAmount+= itemInfo.price * cartItems[item];        
+                    }
+                            }
+        
+          return totalAmount;
+                        }
+    
     
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount,
     }
 
     return (
